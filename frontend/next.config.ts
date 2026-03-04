19170d80441b2fecf182e5e84c0efb34da2e6474
Dagmawi Delelegne <ddelele1@umbc.edu>
2026-03-04 16:56:47 -0500

    Allow remote image hosts in Next.js config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "encrypted-tbn3.gstatic.com" },
-      // you might also need these depending on your mock urls:
-      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
-      { protocol: "https", hostname: "encrypted-tbn1.gstatic.com" },
-      { protocol: "https", hostname: "encrypted-tbn2.gstatic.com" },
-      { protocol: "https", hostname: "www.imdb.com" },
-      { protocol: "https", hostname: "en.wikipedia.org" },
+      { protocol: "https", hostname: "image.tmdb.org", pathname: "/t/p/**" },
     ],
   },
 };
