c81fcd8944608b32c06281eab9937cba8611b5fd
Dagmawi Delelegne <ddelele1@umbc.edu>
2026-04-08 17:15:56 -0400

    Added thumbnail url to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org", pathname: "/t/p/**" },
+      { protocol: "https", hostname: "myanimelist.cdn-dena.com", pathname: "/images/**" },
+      
     ],
   },
 };
