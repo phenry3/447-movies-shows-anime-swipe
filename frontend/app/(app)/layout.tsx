096276f7d715c4704565d70157cebabecd1551ce
Dagmawi Delelegne <ddelele1@umbc.edu>
2026-04-08 13:30:48 -0400

    Add auth setup and session components

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,6 +1,30 @@
+"use client"
+
 import Navigation from "@/components/Navigation";
+import {useSession} from "next-auth/react"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return null;
+  }
+  
+  if (!session) {
+    return null;
+  }
+  
   return (
     <main className="min-h-screen bg-black text-white pt-20">
       <Navigation/>
---
+++ frontend/components/LoginButtons.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { signIn} from "next-auth/react";
+
+export default function LoginButtons() {
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center">
+      <h1 className="text-3xl font-semibold text-white">FilmFlicks</h1>
+      <button
+        onClick={() => signIn("google", { callbackUrl: "/discovery" })}
+        className="rounded-full bg-white px-6 py-3 text-black font-medium"
+      >
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
