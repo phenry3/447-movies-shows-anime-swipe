e25688228e0f3b5e3c22784f864707578dd28ff0
Dagmawi Delelegne <ddelele1@umbc.edu>
2026-04-28 13:47:35 -0400

    allow myanimelist.net images in next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org", pathname: "/t/p/**" },
       { protocol: "https", hostname: "myanimelist.cdn-dena.com", pathname: "/images/**" },
-      
+      { protocol: "https", hostname: "myanimelist.net", pathname: "/images/**" },
     ],
   },
 };
