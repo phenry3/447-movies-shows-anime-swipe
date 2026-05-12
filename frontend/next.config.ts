7130cac6d4c31db93874929039922fad607bfcee
Dagmawi Delelegne <ddelele1@umbc.edu>
2026-05-12 19:57:02 -0400

    added local host to next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org", pathname: "/t/p/**" },
       { protocol: "https", hostname: "myanimelist.cdn-dena.com", pathname: "/images/**" },
-      
+      { protocol: "http", hostname: "localhost", port: "3000" }
     ],
   },
 };
